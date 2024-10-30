b97042ed5127f7fdeb9492a9ea337282b8b47dbe
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-30 22:27:27 +0000

    Use updated library

--- src/main/java/dev/enderman/minecraft/plugins/scp/items/SCP018Item.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/items/SCP018Item.kt
@@ -2,18 +2,8 @@ package dev.enderman.minecraft.plugins.scp.items
 
 import dev.enderman.minecraft.plugins.scp.SCPPlugin
 import foundation.esoteric.minecraft.plugins.library.item.CustomItem
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Material
-import org.bukkit.entity.Player
-import org.bukkit.inventory.ItemStack
 
 class SCP018Item(plugin: SCPPlugin) : CustomItem(plugin, "scp_018", Material.SNOWBALL) {
-  override fun generateCustomItem(baseCustomItem: ItemStack, player: Player): ItemStack {
-    baseCustomItem.editMeta { meta ->
-      meta.displayName(Component.text("SCP-018").color(NamedTextColor.RED))
-    }
 
-    return baseCustomItem
-  }
 }

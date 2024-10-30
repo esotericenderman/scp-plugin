63bb9500d3b2bc950eb188df42552822d9e3f815
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-30 19:41:10 +0000

    Add basic SCP-018 item

--- src/main/java/dev/enderman/minecraft/plugins/scp/SCPPlugin.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/SCPPlugin.kt
@@ -2,10 +2,17 @@ package dev.enderman.minecraft.plugins.scp
 
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
+import foundation.esoteric.minecraft.plugins.library.item.CustomItemManager
+import foundation.esoteric.minecraft.plugins.library.item.CustomItemPlugin
 import org.bukkit.plugin.java.JavaPlugin
 
-class SCPPlugin : JavaPlugin() {
+class SCPPlugin : JavaPlugin(), CustomItemPlugin {
+
+    override lateinit var customItemManager : CustomItemManager
+
     override fun onEnable() {
+        customItemManager = CustomItemManager(this)
+
         dataFolder.mkdir()
         saveDefaultConfig()
 

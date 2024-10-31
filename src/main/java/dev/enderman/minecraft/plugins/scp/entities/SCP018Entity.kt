5cdfff785226b754d50d4b2526d3c36d4503ed49
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-31 23:47:03 +0000

    Reflect SCP-018 velocity when it hits the ground

--- src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
@@ -2,11 +2,14 @@ package dev.enderman.minecraft.plugins.scp.entities
 
 import dev.enderman.minecraft.plugins.scp.SCPPlugin
 import foundation.esoteric.minecraft.plugins.library.entity.CustomEntity
+import org.bukkit.block.BlockFace
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.entity.Snowball
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.ProjectileHitEvent
 import org.bukkit.event.entity.ProjectileLaunchEvent
+import org.bukkit.util.Vector
 
 class SCP018Entity(plugin: SCPPlugin) : CustomEntity<Snowball>(plugin, "scp_018", EntityType.SNOWBALL) {
   @EventHandler
@@ -27,6 +30,32 @@ class SCP018Entity(plugin: SCPPlugin) : CustomEntity<Snowball>(plugin, "scp_018"
 
     if ((plugin as SCPPlugin).customItemManager.getItem("scp_018")!!.isItem(itemInMainHand)) {
       toEntity(entity)
+    }
+  }
+
+  @EventHandler
+  fun onHit(event: ProjectileHitEvent) {
+    val projectile = event.entity
+
+    if (!isEntity(projectile)) {
+      return
+    }
+
+    val hitBlockFace = event.hitBlockFace
+    if (hitBlockFace != null) {
+      val velocity = projectile.velocity
+
+      val newVelocity = when (hitBlockFace) {
+        BlockFace.UP, BlockFace.DOWN -> velocity.multiply(Vector(1.0, -1.0, 1.0))
+        BlockFace.NORTH, BlockFace.SOUTH -> velocity.multiply(Vector(1.0, 1.0, -1.0))
+        BlockFace.EAST, BlockFace.WEST -> velocity.multiply(Vector(-1.0, 1.0, 1.0))
+        else -> velocity
+      }
+
+      val newProjectile = createEntity(projectile.location)
+      newProjectile.velocity = newVelocity
+
+      projectile.remove()
     }
   }
 }

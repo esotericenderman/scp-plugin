d1828eb611d8fbb2fe2821dd7238ce7137d8a8ba
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-31 23:34:59 +0000

    Detect when SCP-018 is thrown

--- src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
@@ -3,8 +3,30 @@ package dev.enderman.minecraft.plugins.scp.entities
 import dev.enderman.minecraft.plugins.scp.SCPPlugin
 import foundation.esoteric.minecraft.plugins.library.entity.CustomEntity
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
 import org.bukkit.entity.Snowball
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.ProjectileLaunchEvent
 
 class SCP018Entity(plugin: SCPPlugin) : CustomEntity<Snowball>(plugin, "scp_018", EntityType.SNOWBALL) {
+  @EventHandler
+  fun onThrow(event: ProjectileLaunchEvent) {
+    val entity = event.entity
 
+    if (entity !is Snowball) {
+      return
+    }
+
+    val source = entity.shooter
+
+    if (source !is Player) {
+      return
+    }
+
+    val itemInMainHand = source.inventory.itemInMainHand
+
+    if ((plugin as SCPPlugin).customItemManager.getItem("scp_018")!!.isItem(itemInMainHand)) {
+      toEntity(entity)
+    }
+  }
 }

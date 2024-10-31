9aaad2487a2a83ede6546064e8dd893ca61ca3b0
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-31 00:19:22 +0000

    Update library

--- src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
@@ -2,13 +2,9 @@ package dev.enderman.minecraft.plugins.scp.entities
 
 import dev.enderman.minecraft.plugins.scp.SCPPlugin
 import foundation.esoteric.minecraft.plugins.library.entity.CustomEntity
-import org.bukkit.Location
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Snowball
 
-class SCP018Entity(plugin: SCPPlugin) : CustomEntity<Snowball>(plugin, "scp_018") {
-  override fun generateCustomEntity(spawnLocation: Location?): List<Snowball> {
-    val snowball = spawnLocation!!.world.spawnEntity(spawnLocation, EntityType.SNOWBALL) as Snowball
-    return listOf(snowball)
-  }
+class SCP018Entity(plugin: SCPPlugin) : CustomEntity<Snowball>(plugin, "scp_018", EntityType.SNOWBALL) {
+
 }

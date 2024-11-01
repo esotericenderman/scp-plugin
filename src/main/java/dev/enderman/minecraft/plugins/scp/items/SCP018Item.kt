fe40548802072a01ce9e2d611c7c48c58e748529
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-01 18:25:21 +0000

    Make SCP-018 a textured item

--- src/main/java/dev/enderman/minecraft/plugins/scp/items/SCP018Item.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/items/SCP018Item.kt
@@ -1,9 +1,9 @@
 package dev.enderman.minecraft.plugins.scp.items
 
 import dev.enderman.minecraft.plugins.scp.SCPPlugin
-import foundation.esoteric.minecraft.plugins.library.item.CustomItem
+import foundation.esoteric.minecraft.plugins.library.item.TexturedItem
 import org.bukkit.Material
 
-class SCP018Item(plugin: SCPPlugin) : CustomItem(plugin, "scp_018", Material.SNOWBALL) {
+class SCP018Item(plugin: SCPPlugin) : TexturedItem(plugin, "scp_018", Material.SNOWBALL) {
 
 }

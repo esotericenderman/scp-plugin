c3e430f65de193bc67b26d4f120bd460c03e5d70
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-02 14:05:03 +0000

    Update SCP-018 collision calculation

--- src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
@@ -2,12 +2,10 @@ package dev.enderman.minecraft.plugins.scp.entities
 
 import dev.enderman.minecraft.plugins.scp.SCPPlugin
 import foundation.esoteric.minecraft.plugins.library.entity.CustomEntity
-import org.bukkit.block.BlockFace
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.Snowball
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.ProjectileHitEvent
-import org.bukkit.util.Vector
 
 class SCP018Entity(plugin: SCPPlugin) : CustomEntity<Snowball>(plugin, "scp_018", EntityType.SNOWBALL) {
   @EventHandler
@@ -18,22 +16,21 @@ class SCP018Entity(plugin: SCPPlugin) : CustomEntity<Snowball>(plugin, "scp_018"
       return
     }
 
-    val hitBlockFace = event.hitBlockFace
-    if (hitBlockFace != null) {
+    val hitEntity = event.hitEntity
+    val hitBlock = event.hitBlock
+
+    if (hitEntity != null) {
+      event.isCancelled = true
       val velocity = projectile.velocity
-
-      val newVelocity = when (hitBlockFace) {
-        BlockFace.UP, BlockFace.DOWN -> velocity.multiply(Vector(1.0, -1.0, 1.0))
-        BlockFace.NORTH, BlockFace.SOUTH -> velocity.multiply(Vector(1.0, 1.0, -1.0))
-        BlockFace.EAST, BlockFace.WEST -> velocity.multiply(Vector(-1.0, 1.0, 1.0))
-        else -> velocity
-      }
-
+      velocity.y *= -2.0F
+      projectile.velocity = velocity
+    } else if (hitBlock != null) {
+      val hitFace = event.hitBlockFace!!
+      val normalVector = hitFace.direction
+      val velocity = projectile.velocity
+      val newVelocity = velocity.subtract(normalVector.multiply(2.0F * velocity.dot(normalVector)))
       val newProjectile = createEntity(projectile.location)
-      newProjectile.item = (plugin as SCPPlugin).customItemManager.getItem("scp_018")!!.toItem(newProjectile.item)
       newProjectile.velocity = newVelocity
-
-      projectile.remove()
     }
   }
 }

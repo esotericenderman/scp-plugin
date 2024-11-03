bb4ce24f939f4ea630d0c880a122d818742ffdc6
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-03 17:10:52 +0000

    Limit SCP-018 velocity to 1 block / tick
    This is an attempt to prevent it glitching into blocks.

--- src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/entities/SCP018Entity.kt
@@ -56,8 +56,8 @@ class SCP018Entity<T : Entity>(plugin: SCPPlugin) : CustomEntity<T>(plugin, "scp
       val velocity = projectile.velocity
       val newVelocity = velocity.subtract(normalVector.multiply(2.0F * velocity.dot(normalVector)))
       newVelocity.multiply(3F/2F)
-      if (newVelocity.length() > 2F) {
-        newVelocity.normalize().multiply(2F)
+      if (newVelocity.length() > 1F) {
+        newVelocity.normalize().multiply(1F)
       }
 
       val newProjectile = createEntity(projectile.location)

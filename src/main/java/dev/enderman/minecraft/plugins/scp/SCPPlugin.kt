a8b9789a9cc64d5fd11108a7a5bdbdf2faa5f488
Esoteric Enderman <esotericenderman@gmail.com>
2025-01-27 21:28:18 +0000

    Add basic SCP-268 system

--- src/main/java/dev/enderman/minecraft/plugins/scp/SCPPlugin.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/SCPPlugin.kt
@@ -2,6 +2,7 @@ package dev.enderman.minecraft.plugins.scp
 
 import dev.enderman.minecraft.plugins.scp.entities.SCP018Entity
 import dev.enderman.minecraft.plugins.scp.items.SCP018Item
+import dev.enderman.minecraft.plugins.scp.items.SCP268Item
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
 import foundation.esoteric.minecraft.plugins.library.commands.GiveCustomItemCommand
@@ -10,6 +11,7 @@ import foundation.esoteric.minecraft.plugins.library.entity.CustomEntityPlugin
 import foundation.esoteric.minecraft.plugins.library.item.CustomItemManager
 import foundation.esoteric.minecraft.plugins.library.item.CustomItemPlugin
 import foundation.esoteric.minecraft.plugins.library.pack.resource.ResourcePackManager
+import gg.flyte.twilight.Twilight
 import org.bukkit.plugin.java.JavaPlugin
 
 class SCPPlugin : JavaPlugin(), CustomItemPlugin, CustomEntityPlugin {
@@ -17,9 +19,13 @@ class SCPPlugin : JavaPlugin(), CustomItemPlugin, CustomEntityPlugin {
   override lateinit var customItemManager: CustomItemManager
   override lateinit var customEntityManager: CustomEntityManager
 
+  lateinit var twilight: Twilight
+
   override fun onEnable() {
     customItemManager = CustomItemManager(this)
     customEntityManager = CustomEntityManager(this)
+
+    twilight = Twilight(this)
 
     dataFolder.mkdir()
     saveDefaultConfig()
@@ -31,6 +37,8 @@ class SCPPlugin : JavaPlugin(), CustomItemPlugin, CustomEntityPlugin {
 
     SCP018Entity(this)
     SCP018Item(this)
+
+    SCP268Item(this)
 
     ResourcePackManager(this)
 
--- src/main/java/dev/enderman/minecraft/plugins/scp/items/SCP268Item.kt
+++ src/main/java/dev/enderman/minecraft/plugins/scp/items/SCP268Item.kt
@@ -1,13 +1,48 @@
 package dev.enderman.minecraft.plugins.scp.items
 
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent
 import dev.enderman.minecraft.plugins.scp.SCPPlugin
 import foundation.esoteric.minecraft.plugins.library.item.TexturedItem
+import gg.flyte.twilight.extension.hidePlayer
+import gg.flyte.twilight.extension.showPlayer
 import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 class SCP268Item(plugin: SCPPlugin) : TexturedItem(plugin, "scp_268", Material.LEATHER_HELMET) {
+  @EventHandler
+  private fun onEquip(event: PlayerArmorChangeEvent) {
+    val player = event.player
 
-  init {
-    println(this::class.qualifiedName.hashCode())
+    val newItem = event.newItem
+    val oldItem = event.oldItem
+
+    if (isItem(newItem)) {
+      plugin.logger.info("[SCP-268] The player has put on the hat... now invisible.")
+      player.hidePlayer(plugin, player)
+      player.hidePlayer()
+
+      player.addPotionEffect(
+        PotionEffect(
+          PotionEffectType.INVISIBILITY,
+          PotionEffect.INFINITE_DURATION,
+          1,
+          false,
+          false,
+          false
+        )
+      )
+    }
+
+    if (isItem(oldItem)) {
+      plugin.logger.info("[SCP-268] The player has taken off the hat... now visible.")
+      player.showPlayer(plugin, player)
+      player.showPlayer()
+
+      player.removePotionEffect(
+        PotionEffectType.INVISIBILITY
+      )
+    }
   }
-
 }
